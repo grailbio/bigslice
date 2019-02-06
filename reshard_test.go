500daeef109d6bc129e388ef2efe887799ada100
Josh Newman <joshnewman@grailbio.com>
2019-02-06 11:07:40 -0800

    bigslice: improve the Reshard test
    Summary:
    Previously (D25000), it checked all output rows were grouped into shards
    legally. Now, it also checks that all input rows appear in the output.
    
    Test Plan: Run the test.
    
    Reviewers: marius
    
    Reviewed By: marius
    
    Differential Revision: https://phabricator.grailbio.com/D25092
    
    fbshipit-source-id: 020b704

--- reshard_test.go
+++ reshard_test.go
@@ -2,57 +2,77 @@ package bigslice_test
 
 import (
 	"context"
+	"fmt"
 	"math/rand"
+	"reflect"
+	"sort"
 	"strings"
 	"testing"
 
 	"github.com/grailbio/bigslice"
+	"github.com/grailbio/bigslice/exec"
 	"github.com/grailbio/bigslice/frame"
 	"github.com/grailbio/bigslice/sliceio"
 )
 
 func TestReshard(t *testing.T) {
-	// Construct a slice with columns:
-	//   1. lengthHashKey (strings that hash to their length), and
-	//   2. same value copied as a []byte
 	const N = 100
 	col1 := make([]lengthHashKey, N)
-	col2 := make([][]byte, N)
+	col2 := make([]int, N)
+	want := map[lengthHashKey][]int{} // map of val1 -> []val2
 	rnd := rand.New(rand.NewSource(0))
 	for i := range col1 {
 		s := strings.Repeat("a", rnd.Intn(N/10)) // We want many strings of each length.
 		col1[i] = lengthHashKey(s)
-		col2[i] = []byte(s)
+		col2[i] = rnd.Intn(100)
+		want[col1[i]] = append(want[col1[i]], col2[i])
+	}
+	for _, vals := range want {
+		sort.Ints(vals) // for equality test later
 	}
 	for m := 1; m < 10; m++ {
-		slice := bigslice.Const(m, append([]lengthHashKey{}, col1...), append([][]byte{}, col2...))
-		slice = bigslice.Reshard(slice)
+		t.Run(fmt.Sprint(m), func(t *testing.T) {
+			slice := bigslice.Const(m, append([]lengthHashKey{}, col1...), append([]int{}, col2...))
+			slice = bigslice.Reshard(slice)
 
-		// map of col1 length -> set of shards that had keys of that length.
-		lengthShards := map[int]map[int]struct{}{}
-		slice = bigslice.Scan(slice, func(shard int, scanner *sliceio.Scanner) error {
+			// map of col1 length -> set of shards that had keys of that length.
+			lengthShards := map[int]map[int]struct{}{}
+			got := map[lengthHashKey][]int{} // map of val1 -> []val2
 			var (
 				val1 lengthHashKey
-				val2 []byte
+				val2 int
 			)
-			for scanner.Scan(context.Background(), &val1, &val2) {
-				if string(val1) != string(val2) {
-					t.Errorf("expected matching val1, val2, got %q, %q", val1, val2)
+			slice = bigslice.Scan(slice, func(shard int, scanner *sliceio.Scanner) error {
+				for scanner.Scan(context.Background(), &val1, &val2) {
+					if _, ok := lengthShards[len(val1)]; !ok {
+						lengthShards[len(val1)] = map[int]struct{}{}
+					}
+					lengthShards[len(val1)][shard] = struct{}{}
+					got[val1] = append(got[val1], val2)
 				}
-				if _, ok := lengthShards[len(val1)]; !ok {
-					lengthShards[len(val1)] = map[int]struct{}{}
-				}
-				lengthShards[len(val1)][shard] = struct{}{}
-			}
-			return scanner.Err()
-		})
-		_ = run(context.Background(), t, slice)
+				return scanner.Err()
+			})
 
-		for length, shards := range lengthShards {
-			if len(shards) != 1 {
-				t.Errorf("found keys of length %d in multiple shards: %v", length, shards)
+			sess := exec.Start(exec.Local)
+			defer sess.Shutdown()
+			_, err := sess.Run(context.Background(), bigslice.Func(func() bigslice.Slice { return slice }))
+			if err != nil {
+				t.Fatalf("run error %v", err)
 			}
-		}
+
+			for length, shards := range lengthShards {
+				if len(shards) != 1 {
+					t.Errorf("found keys of length %d in multiple shards: %v", length, shards)
+				}
+			}
+
+			for _, vals := range got {
+				sort.Ints(vals)
+			}
+			if !reflect.DeepEqual(got, want) {
+				t.Errorf("got %v, want %v", got, want)
+			}
+		})
 	}
 }
 
