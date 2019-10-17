64d9771fbc32acd3bb0a9e46d2d177b33739fb86
Cosmos Nicolaou <cosmos.nicolaou@gmail.com>
2019-10-17 03:40:34 +0000

    .

---
+++ cmd/bigslice/bigslicecmd/run.go
@@ -0,0 +1,32 @@
+package bigslicecmd
+
+import (
+	"context"
+	"os"
+	"os/exec"
+
+	"github.com/grailbio/base/must"
+)
+
+// RunUsage is the usage message for the Run command.
+const RunUsage = `usage: bigslice run [input] [flags]
+
+Command run builds and then runs the provided package or files. See
+"bigslice build -help" for more details.
+`
+
+// Run executes the supplied arguments as a subprocess. If no arguments are
+// supplied, Build(ctx, ".") is used to build the current package and run that.
+func Run(ctx context.Context, args []string) {
+	if len(args) == 0 {
+		binary = Build(ctx, []string{"."}, "")
+	} else {
+		binary = args[0]
+	}
+	cmd := exec.CommandContext(ctx, binary, args[1:]...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+	must.Nil(cmd.Run())
+	must.Nil(os.Remove(binary))
+}
