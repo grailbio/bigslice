37ad6f6d8d74faaa000ecd2508c9e0ec9955f35e
Jaran Charumilind <jcharumilind@grailbio.com>
2019-11-04 14:35:20 -0800

    bigslice/exec: fix intra-invocation task reuse
    Summary:
    Fix intra-invocation task reuse to work properly with downstream shuffles. The existing implementation failed to take into account the fact that tasks depend on their downstream usage, for example because of map-side reducing or different subsequent shard numbers.  This meant that tasks would get reused inappropriately.
    
    To make this a bit more clear, the code now produces tasks immutable w.r.t. to compilation. Previously, compilation would modify dependees. Instead, all reuse behavior is now handled during task compilation.
    
    Test Plan: Make and run unit tests; run `classifier-evaluation`
    
    Reviewers: pgopal, marius, smahadevan, joshnewman
    
    Reviewed By: marius
    
    Differential Revision: https://phabricator.grailbio.com/D38717
    
    fbshipit-source-id: 6fd6d8f

--- exec/compile.go
+++ exec/compile.go
@@ -6,6 +6,7 @@ package exec
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 
 	"github.com/grailbio/base/log"
@@ -38,6 +39,39 @@ func pipeline(slice bigslice.Slice) (slices []bigslice.Slice) {
 	}
 }
 
+// memoKey is the memo key for memoized slice compilations.
+type memoKey struct {
+	slice bigslice.Slice
+	// numPartition is the number of partitions in the output of the memoized
+	// compiled tasks.
+	numPartition int
+}
+
+// partitioner configures the output partitioning of compiled tasks. The zero
+// value indicates that the output of the tasks are not for a shuffle
+// dependency.
+type partitioner struct {
+	// numPartition is the number of partitions in the output for a shuffle
+	// dependency, if >1. If 0, the output is not used by a shuffle.
+	numPartition int
+	Combiner     *reflect.Value
+	CombineKey   string
+}
+
+// IsShuffle returns whether the task output is used by a shuffle dependency.
+func (p partitioner) IsShuffle() bool {
+	return p.numPartition != 0
+}
+
+// NumPartition returns the number of partitions that the task output should
+// have. If this is not a shuffle dependency, returns 1.
+func (p partitioner) NumPartition() int {
+	if p.numPartition == 0 {
+		return 1
+	}
+	return p.numPartition
+}
+
 // Compile compiles the provided slice into a set of task graphs,
 // each representing the computation for one shard of the slice. The
 // slice is produced by the provided invocation. Compile coalesces
@@ -57,8 +91,11 @@ func pipeline(slice bigslice.Slice) (slices []bigslice.Slice) {
 // all other slices must be derived. This simplifies the
 // implementation but may make the API a little confusing.
 func compile(slice bigslice.Slice, inv bigslice.Invocation, machineCombiners bool) (tasks []*Task, err error) {
-	c := compiler{make(taskNamer), inv, machineCombiners, make(map[bigslice.Slice][]*Task)}
-	tasks, _, err = c.compile(slice)
+	c := compiler{make(taskNamer), inv, machineCombiners, make(map[memoKey][]*Task)}
+	// Top-level compilation always produces tasks that write single partitions,
+	// as they are materialized and will not be used as direct shuffle
+	// dependencies.
+	tasks, err = c.compile(slice, partitioner{})
 	return
 }
 
@@ -66,40 +103,87 @@ type compiler struct {
 	namer            taskNamer
 	inv              bigslice.Invocation
 	machineCombiners bool
-	memo             map[bigslice.Slice][]*Task
+	memo             map[memoKey][]*Task
 }
 
-// compile_ compiles the provided slice into a set of task graphs,
-// memoizing the compilation so that tasks can be reused within the invocation.
-func (c *compiler) compile(slice bigslice.Slice) (tasks []*Task, reused bool, err error) {
+// compile compiles the provided slice into a set of task graphs, memoizing the
+// compilation so that tasks can be reused within the invocation.
+func (c *compiler) compile(slice bigslice.Slice, part partitioner) (tasks []*Task, err error) {
+	// We never reuse combiner tasks, as we currently don't have a way of
+	// identifying equivalent combiner functions.
+	if part.Combiner == nil {
+		// TODO(jcharumilind): Repartition already-computed data instead of
+		// forcing recomputation of the slice if we get a different
+		// numPartition.
+		key := memoKey{slice: slice, numPartition: part.numPartition}
+		if memoTasks, ok := c.memo[key]; ok {
+			// We're compiling the same slice with the same number of partitions
+			// (and no combiner), so we can safely reuse the tasks.
+			return memoTasks, nil
+		}
+		defer func() {
+			if err != nil {
+				return
+			}
+			c.memo[key] = tasks
+		}()
+	}
+	// Beyond this point, any tasks used for shuffles are new and need to have
+	// task groups set up for phasic evaluation.
+	defer func() {
+		if part.IsShuffle() {
+			for _, task := range tasks {
+				task.Group = tasks
+			}
+		}
+	}()
 	// Reuse tasks from a previous invocation.
 	if result, ok := bigslice.Unwrap(slice).(*Result); ok {
-		return result.tasks, true, nil
-	}
-	if tasks, ok := c.memo[slice]; ok {
-		return tasks, true, nil
-	}
-	defer func() {
-		if err != nil {
+		for _, task := range result.tasks {
+			if task.Combiner != nil {
+				// TODO(marius): we may consider supporting this, but it should
+				// be very rare, since it requires the user to explicitly reuse
+				// an intermediate slice, which is impossible via the current
+				// API.
+				return nil, fmt.Errorf("cannot reuse task %s with combine key %s", task, task.CombineKey)
+			}
+		}
+		if !part.IsShuffle() {
+			tasks = result.tasks
 			return
 		}
-		c.memo[slice] = tasks
-	}()
-	// slices is the set of slices that comprise the compiled tasks. len(slices)
-	// may be >1 due to pipelining.
-	slices := []bigslice.Slice{slice}
+		// We now insert a set of tasks whose only purpose is (re-)shuffling
+		// the output from the previously completed task.
+		shuffleOpName := c.namer.New(fmt.Sprintf("%s_shuffle", result.tasks[0].Name.Op))
+		tasks = make([]*Task, len(result.tasks))
+		for shard, task := range result.tasks {
+			tasks[shard] = &Task{
+				Type:       slice,
+				Invocation: c.inv,
+				Name: TaskName{
+					Op:       shuffleOpName,
+					Shard:    shard,
+					NumShard: len(result.tasks),
+				},
+				Do:     func(readers []sliceio.Reader) sliceio.Reader { return readers[0] },
+				Deps:   []TaskDep{{task, 0, false, ""}},
+				Pragma: task.Pragma,
+				Slices: task.Slices,
+			}
+		}
+		return
+	}
+	// Pipeline slices and create a task for each underlying shard, pipelining
+	// the eligible computations.
+	slices := pipeline(slice)
 	defer func() {
-		for _, t := range tasks {
-			t.Slices = slices
+		for _, task := range tasks {
+			task.Slices = slices
 		}
 	}()
-	// Pipeline slices and create a task for each underlying shard,
-	// pipelining the eligible computations.
-	tasks = make([]*Task, slice.NumShard())
-	slices = pipeline(slice)
+	var pragmas bigslice.Pragmas
 	ops := make([]string, 0, len(slices)+1)
 	ops = append(ops, fmt.Sprintf("inv%d", c.inv.Index))
-	var pragmas bigslice.Pragmas
 	for i := len(slices) - 1; i >= 0; i-- {
 		ops = append(ops, slices[i].Name().Op)
 		if pragma, ok := slices[i].(bigslice.Pragma); ok {
@@ -107,97 +191,50 @@ func (c *compiler) compile(slice bigslice.Slice) (tasks []*Task, reused bool, er
 		}
 	}
 	opName := c.namer.New(strings.Join(ops, "_"))
+	tasks = make([]*Task, slice.NumShard())
 	for i := range tasks {
 		tasks[i] = &Task{
 			Type:         slices[0],
 			Name:         TaskName{Op: opName, Shard: i, NumShard: len(tasks)},
 			Invocation:   c.inv,
-			NumPartition: 1,
 			Pragma:       pragmas,
+			NumPartition: part.NumPartition(),
+			Combiner:     part.Combiner,
+			CombineKey:   part.CombineKey,
 		}
 	}
-	// Capture the dependencies for this task set;
-	// they are encoded in the last slice.
+	// Capture the dependencies for this task set; they are encoded in the last
+	// slice.
 	lastSlice := slices[len(slices)-1]
 	for i := 0; i < lastSlice.NumDep(); i++ {
 		dep := lastSlice.Dep(i)
-		deptasks, reused, err := c.compile(dep.Slice)
-		if err != nil {
-			return nil, false, err
-		}
-		// These needn't be shuffle deps, for example if we terminated
-		// pipelining early because we're reusing a result or because we're
-		// doing a shuffle-free join.
 		if !dep.Shuffle {
-			if len(tasks) != len(deptasks) {
-				log.Panicf("tasks:%d deptasks:%d", len(tasks), len(deptasks))
+			depTasks, err := c.compile(dep.Slice, partitioner{})
+			if err != nil {
+				return nil, err
+			}
+			if len(tasks) != len(depTasks) {
+				log.Panicf("tasks:%d deptasks:%d", len(tasks), len(depTasks))
 			}
 			for shard := range tasks {
 				tasks[shard].Deps = append(tasks[shard].Deps,
-					TaskDep{deptasks[shard], 0, dep.Expand, ""})
+					TaskDep{depTasks[shard], 0, dep.Expand, ""})
 			}
 			continue
 		}
-
-		// In the case where we are reusing slice results and require a
-		// shuffle, we have to insert an explicit shuffle stage. This is
-		// done by creating a pass-thru task for each dependency task.
-		// These tasks then receive a shuffle dependency from the task set
-		// we are compiling. This in turn will induce shuffling and local
-		// combining at runtime.
-		if reused {
-			for _, task := range deptasks {
-				if task.Combiner != nil {
-					// TODO(marius): we may consider supporting this, but it should
-					// be very rare, since it requires the user to explicitly reuse
-					// an intermediate slice, which is impossible via the current
-					// API.
-					return nil, false, fmt.Errorf("cannot reuse task %s with combine key %s", task, task.CombineKey)
-				}
-			}
-			newDeps := make([]*Task, len(deptasks))
-			// We now insert a set of tasks whose only purpose is (re-)shuffling
-			// the output from the previously completed task.
-			for shard, task := range deptasks {
-				newDeps[shard] = &Task{
-					Type:       dep.Slice,
-					Invocation: c.inv,
-					Name: TaskName{
-						Op:       fmt.Sprintf("%s_shuffle_%d", opName, i),
-						Shard:    shard,
-						NumShard: len(deptasks),
-					},
-					Do:     func(readers []sliceio.Reader) sliceio.Reader { return readers[0] },
-					Deps:   []TaskDep{{task, 0, false, ""}},
-					Pragma: task.Pragma,
-				}
-			}
-			deptasks = newDeps
-		}
-
-		for _, task := range deptasks {
-			task.Group = deptasks
-		}
-
 		var combineKey string
 		if lastSlice.Combiner() != nil && c.machineCombiners {
 			combineKey = opName
 		}
-		// Assign a partitioner and partition width our dependencies, so that
-		// these are properly partitioned at the time of computation.
-		for _, task := range deptasks {
-			task.NumPartition = slice.NumShard()
-			// Assign a combine key that's based on the root name of the task.
-			// This is a name that's unique in the task namespace and is used to
-			// coalesce combiners on a single machine.
-			task.Combiner = lastSlice.Combiner()
-			task.CombineKey = combineKey
+		depPart := partitioner{slice.NumShard(), lastSlice.Combiner(), combineKey}
+		depTasks, err := c.compile(dep.Slice, depPart)
+		if err != nil {
+			return nil, err
 		}
-
 		// Each shard reads different partitions from all of the previous slice's shards.
 		for partition := range tasks {
 			tasks[partition].Deps = append(tasks[partition].Deps,
-				TaskDep{deptasks[0], partition, dep.Expand, combineKey})
+				TaskDep{depTasks[0], partition, dep.Expand, combineKey})
 		}
 	}
 	// Pipeline execution, folding multiple frame operations
@@ -239,7 +276,7 @@ func (c *compiler) compile(slice bigslice.Slice) (tasks []*Task, reused bool, er
 			}
 		}
 	}
-	return tasks, false, nil
+	return
 }
 
 type taskNamer map[string]int
--- exec/compile_test.go
+++ exec/compile_test.go
@@ -5,129 +5,283 @@
 package exec
 
 import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"sort"
+	"strings"
 	"testing"
 
 	"github.com/grailbio/bigslice"
 )
 
-// TestMaterialize verifies that the Materialize pragma interrupts pipeline,
-// which subsequently causes task results to be materialized.
-func TestMaterialize(t *testing.T) {
-	const N = 100
-	f := bigslice.Func(func() (slice bigslice.Slice) {
-		slice = makeMaterializeReader(N)
-		slice = bigslice.Map(slice, func(i int) int { return i })
-		return
-	})
-	// Expect N*2 tasks:
-	// N for the reader
-	// N for the map
-	//
-	// Without the Materialize pragma, this would have N tasks, as the reader
-	// and map would be pipelined together.
-	if got, want := countTasks(t, f), N*2; got != want {
-		t.Errorf("got %v, want %v", got, want)
+func TestCompile(t *testing.T) {
+	for _, c := range []struct {
+		name string
+		f    func() bigslice.Slice
+	}{
+		{
+			"trivial",
+			func() (slice bigslice.Slice) {
+				slice = bigslice.Const(3, []int{})
+				return
+			},
+		},
+		{
+			"shuffle",
+			func() (slice bigslice.Slice) {
+				slice = bigslice.Const(3, []int{}, []float64{})
+				slice = bigslice.Reduce(slice, func(v0, v1 float64) float64 { return v0 + v1 })
+				return
+			},
+		},
+		{
+			// Branch where both branches pipeline with the subsequent maps.
+			"branch",
+			func() (slice bigslice.Slice) {
+				slice = bigslice.Const(3, []int{})
+				slice = bigslice.Map(slice, func(i int) int { return i })
+				slice0 := bigslice.Map(slice, func(i int) int { return i })
+				slice1 := bigslice.Map(slice, func(i int) int { return i })
+				slice = bigslice.Cogroup(slice0, slice1)
+				return
+			},
+		},
+		{
+			// Branch from a materialized slice, so the subsequent maps are not
+			// pipelined through the materialized tasks.
+			"branch-materialize",
+			func() (slice bigslice.Slice) {
+				slice = bigslice.Const(3, []int{})
+				slice = bigslice.Map(slice, func(i int) int { return i }, bigslice.ExperimentalMaterialize)
+				slice0 := bigslice.Map(slice, func(i int) int { return i })
+				slice1 := bigslice.Map(slice, func(i int) int { return i })
+				slice = bigslice.Cogroup(slice0, slice1)
+				return
+			},
+		},
+		{
+			// Branch the const slice with a reduce, which introduces its own
+			// shuffle/combiner, so the const slice tasks cannot be reused.
+			"branch-shuffle",
+			func() (slice bigslice.Slice) {
+				slice = bigslice.Const(3, []int{}, []float64{})
+				slice0 := bigslice.Reduce(slice, func(v0, v1 float64) float64 { return v0 + v1 })
+				slice = bigslice.Cogroup(slice, slice0)
+				return
+			},
+		},
+		{
+			// Branch where each branch demands the same partition number from
+			// the branch point slice. In this case, the branch point tasks can
+			// be reused.
+			"branch-same-partitions",
+			func() (slice bigslice.Slice) {
+				slice = bigslice.Const(3, []int{})
+				slice = bigslice.Map(slice, func(i int) int { return i })
+				slice0 := bigslice.Reshard(slice, 2)
+				slice1 := bigslice.Reshard(slice, 2)
+				slice = bigslice.Cogroup(slice0, slice1)
+				return
+			},
+		},
+		{
+			// Branch where each branch demands different partition numbers from
+			// the branch point slice. In this case, the branch point tasks
+			// cannot be reused.
+			"branch-different-partitions",
+			func() (slice bigslice.Slice) {
+				slice = bigslice.Const(3, []int{})
+				slice = bigslice.Map(slice, func(i int) int { return i })
+				slice0 := bigslice.Reshard(slice, 1)
+				slice1 := bigslice.Reshard(slice, 2)
+				slice = bigslice.Cogroup(slice0, slice1)
+				return
+			},
+		},
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			inv := bigslice.Func(c.f).Invocation("<unknown>")
+			inv.Index = 1
+			slice := inv.Invoke()
+			tasks, err := compile(slice, inv, false)
+			if err != nil {
+				t.Fatalf("compilation failed")
+			}
+			iterTasks(tasks, func(task *Task) {
+				if task.Pragma == nil {
+					t.Errorf("%v has nil task.Pragma", task)
+				}
+			})
+			g := makeGraph(tasks)
+			want, err := ioutil.ReadFile("testdata/" + c.name + ".graph")
+			if err != nil {
+				t.Fatalf("error reading graph: %v", err)
+			}
+			d := lineDiff(g.String(), string(want))
+			if d != "" {
+				t.Errorf("differs from %s.graph:\n%s", c.name, d)
+			}
+		})
 	}
+
 }
 
-// TestReuse verifies that results from a slice with the Materialize pragma are
-// reused (i.e. downstream tasks refer to the same tasks with materialized
-// results).
-func TestReuse(t *testing.T) {
-	const N = 100
-	diamond := bigslice.Func(func() (slice bigslice.Slice) {
-		slice = makeMaterializeReader(N)
-		slice = bigslice.Map(slice, func(i int) int { return i })
-		lhsSlice := bigslice.Map(slice, func(i int) int { return i })
-		rhsSlice := bigslice.Map(slice, func(i int) int { return i })
-		slice = bigslice.Cogroup(lhsSlice, rhsSlice)
-		return
-	})
-	inv := diamond.Invocation("<unknown>")
-	slice := inv.Invoke()
-	tasks, err := compile(slice, inv, false)
-	if err != nil {
-		t.Fatalf("compilation failed")
-	}
-	var numTasks int
-	iterTasks(tasks, func(task *Task) {
-		numTasks++
-	})
-	// Expect N*4 tasks:
-	// N for the cogroup
-	// N for each of the two cogrouped slices
-	// N for the reader
-	//
-	// Without the Materialize pragma, this would have N*3 tasks, as the two
-	// middle map slices would each be independently pipelined with the root
-	// reader.
-	if got, want := numTasks, N*4; got != want {
-		t.Errorf("got %v, want %v", got, want)
-	}
-}
-
-// TestPragma verifies that the Pragma field is set for all compiled tasks.
-func TestPragma(t *testing.T) {
-	inv := reuseWithShuffle.Invocation("<unknown>")
-	slice := inv.Invoke()
-	tasks, err := compile(slice, inv, false)
-	if err != nil {
-		t.Fatal("compilation failed")
-	}
-	iterTasks(tasks, func(task *Task) {
-		if task.Pragma == nil {
-			t.Errorf("%v has nil task.Pragma", task)
+// makeGraph returns a graph representation of the task graph roots that is
+// convenient for printing and comparing. We use this to verify (and debug)
+// compilation results.
+func makeGraph(roots []*Task) graph {
+	var (
+		visited = make(map[*Task]bool)
+		g       graph
+		walk    func(tasks []*Task)
+	)
+	walk = func(tasks []*Task) {
+		if len(tasks) == 0 {
+			return
 		}
-	})
-}
+		for _, t := range tasks {
+			if visited[t] {
+				continue
+			}
+			visited[t] = true
+			g.nodes = append(g.nodes, t.Name.String())
+			for _, d := range t.Deps {
+				for i := 0; i < d.NumTask(); i++ {
+					edge := edge{t.Name.String(), d.Task(i).Name.String()}
+					g.edges = append(g.edges, edge)
+					walk([]*Task{d.Task(i)})
+				}
+			}
 
-var reuseWithShuffle = bigslice.Func(func() (slice bigslice.Slice) {
-	const N = 100
-	colA := make([]int, N)
-	colB := make([]int, N)
-	for i := range colA {
-		colA[i] = i
-		colB[i] = N - i
-	}
-	slice = bigslice.Const(4, colA, colB)
-	slice = bigslice.Map(slice, func(a, b int) (int, int) {
-		return a, b
-	}, bigslice.ExperimentalMaterialize)
-	branch0 := bigslice.Map(slice, func(a, b int) (int, int) {
-		return a, b
-	})
-	// branch1 will reuse the compiled tasks of slice but requires a shuffle.
-	// This introduces a new layer of tasks.
-	branch1 := bigslice.Reduce(slice, func(b0, b1 int) int {
-		return b0 + b1
-	})
-	slice = bigslice.Cogroup(branch0, branch1)
-	return
-})
-
-func makeMaterializeReader(numShards int) bigslice.Slice {
-	return bigslice.ReaderFunc(numShards, func(shard int, x *int, xs []int) (int, error) {
-		var i int
-		for i < len(xs) && *x < 1000 {
-			xs[i] = i
-			i++
-			*x++
 		}
-		return i, nil
-	}, bigslice.ExperimentalMaterialize)
+	}
+	walk(roots)
+	g.Sort()
+	return g
 }
 
-func countTasks(t *testing.T, f *bigslice.FuncValue) int {
-	t.Helper()
-	inv := f.Invocation("<unknown>")
-	slice := inv.Invoke()
-	tasks, err := compile(slice, inv, false)
-	if err != nil {
-		t.Fatal("compilation failed")
-	}
-	var numTasks int
-	iterTasks(tasks, func(task *Task) {
-		numTasks++
+type edge struct {
+	src string
+	dst string
+}
+
+type graph struct {
+	nodes []string
+	edges []edge
+}
+
+func (g graph) Sort() {
+	sort.Sort(sort.StringSlice(g.nodes))
+	sort.Slice(g.edges, func(i, j int) bool {
+		if g.edges[i].src != g.edges[j].src {
+			return g.edges[i].src < g.edges[j].src
+		}
+		return g.edges[i].dst < g.edges[j].dst
 	})
-	return numTasks
+}
+
+func (g graph) String() string {
+	var b bytes.Buffer
+	for _, n := range g.nodes {
+		fmt.Fprintf(&b, "%s\n", n)
+	}
+	for _, e := range g.edges {
+		fmt.Fprintf(&b, "%s -> %s\n", e.src, e.dst)
+	}
+	return b.String()
+}
+
+func lineDiff(lhs, rhs string) string {
+	lhsLines := strings.Split(lhs, "\n")
+	rhsLines := strings.Split(rhs, "\n")
+
+	// This is a vanilla Levenshtein distance implementation.
+	const (
+		editNone = iota
+		editAdd
+		editDel
+		editRep
+	)
+	type cell struct {
+		edit int
+		cost int
+	}
+	cells := make([][]cell, len(lhsLines)+1)
+	for i := range cells {
+		cells[i] = make([]cell, len(rhsLines)+1)
+	}
+	for i := 1; i < len(lhsLines)+1; i++ {
+		cells[i][0].edit = editDel
+		cells[i][0].cost = i
+	}
+	for j := 1; j < len(rhsLines)+1; j++ {
+		cells[0][j].edit = editAdd
+		cells[0][j].cost = j
+	}
+	for i := 1; i < len(lhsLines)+1; i++ {
+		for j := 1; j < len(rhsLines)+1; j++ {
+			if lhsLines[i-1] == rhsLines[j-1] {
+				cells[i][j].cost = cells[i-1][j-1].cost
+				continue
+			}
+			repCost := cells[i-1][j-1].cost + 1
+			minCost := repCost
+			delCost := cells[i-1][j].cost + 1
+			if delCost < minCost {
+				minCost = delCost
+			}
+			addCost := cells[i][j-1].cost + 1
+			if addCost < minCost {
+				minCost = addCost
+			}
+			cells[i][j].cost = minCost
+			switch minCost {
+			case repCost:
+				cells[i][j].edit = editRep
+			case addCost:
+				cells[i][j].edit = editAdd
+			case delCost:
+				cells[i][j].edit = editDel
+			}
+		}
+	}
+	var (
+		d      []string
+		differ bool
+	)
+	for i, j := len(lhsLines), len(rhsLines); i > 0 || j > 0; {
+		switch cells[i][j].edit {
+		case editNone:
+			d = append(d, lhsLines[i-1])
+			i--
+			j--
+		case editAdd:
+			d = append(d, "+ "+rhsLines[j-1])
+			j--
+			differ = true
+		case editDel:
+			d = append(d, "- "+lhsLines[i-1])
+			i--
+			differ = true
+		case editRep:
+			d = append(d, "+ "+rhsLines[j-1])
+			d = append(d, "- "+lhsLines[i-1])
+			i--
+			j--
+			differ = true
+		}
+	}
+	if !differ {
+		return ""
+	}
+	for i := len(d)/2 - 1; i >= 0; i-- {
+		opp := len(d) - 1 - i
+		d[i], d[opp] = d[opp], d[i]
+	}
+	var b bytes.Buffer
+	for _, dLine := range d {
+		b.WriteString(dLine + "\n")
+	}
+	return b.String()
 }
