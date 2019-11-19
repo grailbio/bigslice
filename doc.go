cf919fd5364d967b479722e6a803b9eac7766e22
marius a. eriksen <marius@monkey.org>
2019-11-18 17:37:29 -0800

    docs: add a note about context plumbing in user-provided functions

--- doc.go
+++ doc.go
@@ -22,17 +22,30 @@
 	executed remotely, bigslice programs have to be written with a few
 	constraints:
 
-		1. All slices must be constructed by bigslice funcs (bigslice.Func),
-		   and all such functions must be instantiated before
-		   bigslice.Start is called. This rule is easy to follow: if funcs
-		   are global variables, and bigslice.Start is called from a
-		   program's main, then the program is compliant.
-		2. The driver program must be compiled on the same GOOS and GOARCH
-		   as the target architecture. When running locally, this is not a
-		   concern, but programs that require distribution must be run from
-		   a linux/amd64 binary.
+	1. All slices must be constructed by bigslice funcs (bigslice.Func), and all
+	such functions must be instantiated before bigslice.Start is called. This
+	rule is easy to follow: if funcs are global variables, and bigslice.Start
+	is called from a program's main, then the program is compliant.
 
-	Some Bigslice operations may be annotated with runtime pragmas:
-	directives for the Bigslice runtime. See Pragma for details.
+	2. The driver program must be compiled on the same GOOS and GOARCH as the
+	target architecture. When running locally, this is not a concern, but
+	programs that require distribution must be run from a linux/amd64 binary.
+	Bigslice also supports the fat binary format implemented by
+	github.com/grailbio/base/fatbin. The bigslice tool
+	(github.com/grailbio/bigslice/cmd/bigslice) uses this package to compile
+	portable fat binaries.
+
+	Some Bigslice operations may be annotated with runtime pragmas: directives
+	for the Bigslice runtime. See Pragma for details.
+
+	User provided functions in Bigslice
+
+	Functions provided to the various bigslice combinators (e.g., bigslice.Map)
+	may take an additional argument of type context.Context. If specified, then
+	the lifetime of the context is tied to that of the underlying bigslice task.
+	Additionally, the context carries a metrics scope
+	(github.com/grailbio/base/bigslice/metrics.Scope) which can be used to update
+	metric values during data processing.
+
 */
 package bigslice
