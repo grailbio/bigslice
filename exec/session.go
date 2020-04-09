3cb776ee3edfb8e38472164ee1b39e3573b658cc
Jaran Charumilind <jcharumilind@grailbio.com>
2020-04-08 17:38:26 -0700

    Close written trace file

--- exec/session.go
+++ exec/session.go
@@ -380,13 +380,20 @@ func (r *Result) open() sliceio.ReadCloser {
 
 func writeTraceFile(tracer *tracer, path string) {
 	w, err := os.Create(path)
+	defer func() {
+		err := w.Close()
+		if err != nil {
+			log.Error.Printf("error closing trace file at %q: %v", path, err)
+			return
+		}
+	}()
 	if err != nil {
-		log.Error.Printf("could not create trace file at %q: %v", path, err)
+		log.Error.Printf("error creating trace file at %q: %v", path, err)
 		return
 	}
 	err = tracer.Marshal(w)
 	if err != nil {
-		log.Error.Printf("could not marshal to trace file at %q: %v", path, err)
+		log.Error.Printf("erro marshaling to trace file at %q: %v", path, err)
 		return
 	}
 }
